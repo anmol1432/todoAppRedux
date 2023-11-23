0b0038fd01a7d4bfaabf648a4994455d09da5749
Anmol Singh <anmolbuttar143205@gmail.com>
2023-11-23 14:13:55 +0530

    remove unsed

--- src/store/reducers/rootReducer.tsx
+++ src/store/reducers/rootReducer.tsx
@@ -23,22 +23,6 @@ export const root = createSlice({
     screen: 'list'
   },
   reducers: {
-    increment: state => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value += 1
-    },
-
-    decrement: state => {
-      state.value -= 1
-    },
-
-    incrementByAmount: (state, action) => {
-      state.value += action.payload
-    },
-
     doneTask: (state, action) => {
       let { _id } = action.payload
       state.tasks = state.tasks.map((data) => {
@@ -77,6 +61,6 @@ export const root = createSlice({
 })
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount, doneTask, changeScreen, addTask, changeDev, deleteTask } = root.actions
+export const { doneTask, changeScreen, addTask, changeDev, deleteTask } = root.actions
 
 export default root.reducer
